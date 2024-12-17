1fa9b9bda9837e819c4a707cd683ff91a8acb6fc
Maria Fernanda <84775618+mfernandasf@users.noreply.github.com>
2024-12-17 18:59:14 +0000

    Código de cadastro das cartas super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,70 @@
+// importando as libs
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// definindo os limites
+#define MAX_CIDADES 32
+#define MAX_NOME 50
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // definindo as variáveis
+    char codigo[10];
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+    int opcao;
+
+    // laço de repetição para cadastro de cidade, exibir dados ou sair
+    while (1)
+    {
+        printf("Escolha uma opção: \n");
+        printf("1. Cadastrar Cidade\n");
+        printf("2. Exibir Dados das Cidades\n");
+        printf("3. Sair\n");
+        printf("Insira a opção desejada: \n");
+        scanf("%d", &opcao);
+
+        if (opcao == 1) {
+            printf("Cadastrar Cidade\n");
+
+            printf("Insira o código da cidade: (EX: A01)");
+            scanf("%s", &codigo);
+
+            printf("Insira o nome da cidade: ");
+            scanf("%s", nome);
+
+            printf("Insira a população da cidade: ");
+            scanf("%f", &populacao);
+
+            printf("Insira a área (km²): ");
+            scanf("%f", &area);
+
+            printf("Insira o PIB: ");
+            scanf("%f", &pib);
+
+            printf("Insira o número de pontos turísticos: ");
+            scanf("%d", &pontos_turisticos);
+
+            printf("Cadastro realizado com sucesso!");
+        }else if(opcao == 2) {
+            printf("DADOS DA CIDADE");
+            printf("Código: %s\n", codigo);
+            printf("Nome: %s\n", nome);
+            printf("População: %d\n", populacao);
+            printf("Área: %.2f km²\n", area);
+            printf("PIB: R$ %.2f\n", pib);
+            printf("Pontos Turísticos: %d\n", pontos_turisticos);
+        }else if(opcao == 3) {
+            printf("Encerrando o programa");
+            break;
+        }else {
+            printf("Opção inválida");
+        }
+    }
 
     return 0;
 }
